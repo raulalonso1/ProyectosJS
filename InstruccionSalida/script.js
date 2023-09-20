000751b0948b1a7c9967d9f3e433688396526be0
Raul Alonso <94985828+raulalonso1@users.noreply.github.com>
2023-09-21 00:37:48 +0200

    Add files via upload

---
+++ InstruccionSalida/script.js
@@ -0,0 +1,7 @@
+function instruccionSalida(){
+    window.alert(2+3);
+    //document.write("<h2>Texto de prueba</h2>")
+    document.getElementById("parrafo").
+    innerHTML= "Parrafo Cambiado";
+    console.log("Texto de prueba de consola JS");
+}
